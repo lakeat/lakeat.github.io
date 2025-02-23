9ff3d13f5b3d7116f215a46e75e2a9b43e099112
Daniel Wei <lakeat@qq.com>
2025-02-23 20:55:09 +0800

    Initial Jekyll build

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,13 @@
+var store = [{
+        "title": "测试一个中文!",
+        "excerpt":"究竟中文好不好使 公式能不能显示出来$a$. 测试了才知道 You’ll find this post in your _posts directory. Go ahead and edit it and re-build the site to see your changes. You can rebuild the site in many different ways, but the most common way is to run jekyll serve, which launches a web server and auto-regenerates...","categories": ["jekyll","update"],
+        "tags": [],
+        "url": "/jekyll/update/2025/02/23/my-first-post.html",
+        "teaser": null
+      },{
+        "title": "Welcome to Jekyll!",
+        "excerpt":"You’ll find this post in your _posts directory. Go ahead and edit it and re-build the site to see your changes. You can rebuild the site in many different ways, but the most common way is to run jekyll serve, which launches a web server and auto-regenerates your site when...","categories": ["jekyll","update"],
+        "tags": [],
+        "url": "/jekyll/update/2025/02/23/welcome-to-jekyll.html",
+        "teaser": null
+      }]
